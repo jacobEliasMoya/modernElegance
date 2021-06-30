851cce108ca99170120c345dc52ca8e6bb73567c
Jacob moya <jemoya1994@gmail.com>
2021-06-30 09:57:25 -0700

    working on navigation selections

--- public/js/hamburgerNav.js
+++ public/js/hamburgerNav.js
@@ -87,12 +87,91 @@ const goToSelection = (evnt) => {
     // evnt.target;
     // console.log(e)
     if(evnt.target.classList.contains('gohome')){
-        if(selectedDirection === 'centerback'){
+        switch(selectedDirection){
+            case 'centerback':
             homeSection.style.transform= 'rotateX(0deg) rotateY(0deg) translateZ(0vh)';
             navSection.style.transform= 'rotateX(0deg) rotateY(0deg) translateZ(100vh)';
             setTimeout(()=>{
                 selectedDirection='center'
             },1500)
+            break;
+            case 'leftback':
+                navSection.style.transform= 'rotateX(0deg) rotateY(0deg) translateZ(100vh)';
+                homeSection.style.transform= 'rotateX(0deg) rotateY(0deg) translateZ(0vh)';
+                workSection.style = 'initial';
+            setTimeout(()=>{
+                selectedDirection='center'
+            },1500)
+            break;
+            case 'rightback':
+                navSection.style.transform= 'rotateX(0deg) rotateY(0deg) translateZ(100vh)';
+                homeSection.style.transform= 'rotateX(0deg) rotateY(0deg) translateZ(0vh)';
+                contactSection.style = 'initial';
+            setTimeout(()=>{
+                selectedDirection='center'
+            },1500)
+            break;
+            case 'upback':
+                navSection.style.transform= 'rotateX(0deg) rotateY(0deg) translateZ(100vh)';
+                homeSection.style.transform= 'rotateX(0deg) rotateY(0deg) translateZ(0vh)';
+                skillsSection.style = 'initial';
+            setTimeout(()=>{
+                selectedDirection='center'
+            },1500)
+            break;
+            case 'downback':
+                navSection.style.transform= 'rotateX(0deg) rotateY(0deg) translateZ(100vh)';
+                homeSection.style.transform= 'rotateX(0deg) rotateY(0deg) translateZ(0vh)';
+                aboutSection.style = 'initial';
+            setTimeout(()=>{
+                selectedDirection='center'
+            },1500)
+            break;
+            
+        }
+    }
+    if(evnt.target.classList.contains('gowork')){
+        switch(selectedDirection){
+            case 'centerback':
+                workSection.style='transform: rotateX(0deg);';
+                homeSection.style='transform: rotateY(-90deg) translate(100%) translateZ(100px);'
+                navSection.style.transform= 'rotateX(0deg) rotateY(0deg) translateZ(100vh)';
+            setTimeout(()=>{
+                selectedDirection='left'
+            },1500)
+            break;
+            case 'leftback':
+                workSection.style.transform= 'rotateX(0deg) rotateY(0deg) translateZ(0vh)';
+                navSection.style.transform= 'rotateX(0deg) rotateY(0deg) translateZ(100vh)';
+                setTimeout(()=>{
+                selectedDirection='left'
+            },1500)
+            break;
+            case 'rightback':
+                navSection.style.transform= 'rotateX(0deg) rotateY(0deg) translateZ(100vh)';
+                workSection.style='transform: rotateX(0deg);';
+                contactSection.style = 'initial';
+            setTimeout(()=>{
+                selectedDirection='left'
+            },1500)
+            break;
+            case 'upback':
+                navSection.style.transform= 'rotateX(0deg) rotateY(0deg) translateZ(100vh)';
+                workSection.style='transform: rotateX(0deg);';
+                skillsSection.style = 'initial';
+            setTimeout(()=>{
+                selectedDirection='left'
+            },1500)
+            break;
+            case 'downback':
+                navSection.style.transform= 'rotateX(0deg) rotateY(0deg) translateZ(100vh)';
+                workSection.style='transform: rotateX(0deg);';
+                aboutSection.style = 'initial';
+            setTimeout(()=>{
+                selectedDirection='left'
+            },1500)
+            break;
+            
         }
     }
 }
