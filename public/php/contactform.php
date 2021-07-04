8691a0b2464dba246094ad16aa6c7fe82ab13ec8
jacob moya <jemoya1994@Gmail.com>
2021-07-04 09:48:14 -0700

    added form adjustments

---
+++ public/php/contactform.php
@@ -0,0 +1,17 @@
+<?php
+    //first PHP program :)
+    if(isset($_POST['submit'])){
+        $mailFrom = $_POST['email'];
+        $message = $_POST['message'];
+
+        $mailTo = "jmoya@jacobmoya.com";
+        $headers = "From: ".$mailFrom;
+        $txt = "You have receieved an e-mail from ".$name.".\n\n".$message; 
+
+        // function declaration (3 necessary perameters)
+        //email sent to, 
+        //subject
+        //message
+        mail($mailTo,$subject,$txt,$headers);
+    }
+?>
