07e0c62de561a4118d685df84d51520bfdc99a60
jacob moya <jemoya1994@Gmail.com>
2021-07-07 21:28:54 -0700

    finished mainly with layout and sections, minor stylings to fix, then responsiveness

--- public/js/projRotate.js
+++ public/js/projRotate.js
@@ -4,39 +4,39 @@ const opaqueNavIcon = document.querySelector('.navigationContainer .fa');
 
 const linkrotate = () =>{
     if(selectedLink.classList.contains('gameprev')){
-        document.querySelector('.varepprev').style= 'transform: translate(90%,50%) rotateY(-40deg) rotateX(30deg);'
-        document.querySelector('.paintedsite').style= 'transform: translate(-190%,50%) rotateY(40deg) rotateX(30deg);'
-        document.querySelector('.intsite').style= 'transform:translate(-190%,-150%) rotateY(40deg) rotateX(-5deg);'
-        document.querySelector('.reactsite').style= 'transform:translate(90%,-150%) rotateY(-40deg) rotateX(-5deg);'
+        document.querySelector('.varepprev').style= 'transform: translate(-20%,-50%) rotateY(-40deg) rotateX(30deg);'
+        document.querySelector('.paintedsite').style= 'transform: translate(20%,-50%) rotateY(40deg) rotateX(30deg);'
+        document.querySelector('.intsite').style= 'transform:translate(20%,50%) rotateY(40deg) rotateX(-5deg);'
+        document.querySelector('.reactsite').style= 'transform:translate(-20%,50%) rotateY(-40deg) rotateX(-5deg);'
 
     }
     if(selectedLink.classList.contains('intsite')){
-        document.querySelector('.varepprev').style= 'transform: translate(90%,50%) rotateY(-70deg) rotateX(30deg);'
-        document.querySelector('.paintedsite').style= 'transform: translate(-190%,50%) rotateY(20deg) rotateX(30deg);'
-        document.querySelector('.reactsite').style= 'transform:translate(90%,-150%) rotateY(-70deg) rotateX(0deg);'
-        document.querySelector('.gameprev').style= 'transform:translate(-50%,-150%) rotateY(-40deg) rotateX(-5deg);'
+        document.querySelector('.varepprev').style= 'transform: translate(-20%,-50%) rotateY(-70deg) rotateX(30deg);'
+        document.querySelector('.paintedsite').style= 'transform: translate(20%,-50%) rotateY(20deg) rotateX(30deg);'
+        document.querySelector('.reactsite').style= 'transform:translate(-20%,50%) rotateY(-70deg) rotateX(0deg);'
+        document.querySelector('.gameprev').style= 'transform:translate(-50%,50%) rotateY(-40deg) rotateX(-5deg);'
 
     }
     if(selectedLink.classList.contains('paintedsite')){
-        document.querySelector('.varepprev').style= 'transform: translate(90%,50%) rotateY(-70deg) rotateX(0deg);'
-        document.querySelector('.intsite').style= 'transform:translate(-190%,-150%) rotateX(-45deg);'
-        document.querySelector('.reactsite').style= 'transform:translate(90%,-150%) rotateY(-70deg) rotateX(-30deg);'
-        document.querySelector('.gameprev').style= 'transform:translate(-50%,-150%) rotateY(-50deg) rotateX(-50deg);'
+        document.querySelector('.varepprev').style= 'transform: translate(-20%,-50%) rotateY(-70deg) rotateX(0deg);'
+        document.querySelector('.intsite').style= 'transform:translate(20%,50%) rotateX(-45deg);'
+        document.querySelector('.reactsite').style= 'transform:translate(-20%,50%) rotateY(-70deg) rotateX(-30deg);'
+        document.querySelector('.gameprev').style= 'transform:translate(-50%,50%) rotateY(-50deg) rotateX(-50deg);'
 
     }
     if(selectedLink.classList.contains('reactsite')){
-        document.querySelector('.varepprev').style= 'transform: translate(90%,50%) rotateY(-20deg) rotateX(30deg);'
-        document.querySelector('.paintedsite').style= 'transform: translate(-190%,50%) rotateY(70deg) rotateX(30deg);'
-        document.querySelector('.intsite').style= 'transform:translate(-190%,-150%) rotateY(70deg) rotateX(0deg);'
-        document.querySelector('.gameprev').style= 'transform:translate(-50%,-150%) rotateY(40deg) rotateX(-5deg);'
+        document.querySelector('.varepprev').style= 'transform: translate(-20%,-50%) rotateY(-20deg) rotateX(30deg);'
+        document.querySelector('.paintedsite').style= 'transform: translate(20%,-50%) rotateY(70deg) rotateX(30deg);'
+        document.querySelector('.intsite').style= 'transform:translate(20%,50%) rotateY(70deg) rotateX(0deg);'
+        document.querySelector('.gameprev').style= 'transform:translate(-50%,50%) rotateY(40deg) rotateX(-5deg);'
 
 
     }    
     if(selectedLink.classList.contains('varepprev')){
-        document.querySelector('.paintedsite').style= 'transform: translate(-190%,50%) rotateY(70deg) rotateX(0deg);'
-        document.querySelector('.intsite').style= 'transform:translate(-190%,-150%) rotateY(70deg) rotateX(-30deg);'
-        document.querySelector('.reactsite').style= 'transform:translate(90%,-150%) rotateX(-45deg);'
-        document.querySelector('.gameprev').style= 'transform:translate(-50%,-150%) rotateY(50deg) rotateX(-50deg);'
+        document.querySelector('.paintedsite').style= 'transform: translate(20%,-50%) rotateY(70deg) rotateX(0deg);'
+        document.querySelector('.intsite').style= 'transform:translate(20%,50%) rotateY(70deg) rotateX(-30deg);'
+        document.querySelector('.reactsite').style= 'transform:translate(-20%,50%) rotateX(-45deg);'
+        document.querySelector('.gameprev').style= 'transform:translate(-50%,50%) rotateY(50deg) rotateX(-50deg);'
     }
 }
 
@@ -52,10 +52,10 @@ projectLinks.forEach(link=>{
 
 projectLinks.forEach(link=>{
     link.addEventListener('mouseleave',()=>{
-        document.querySelector('.varepprev').style= 'transform: translate(90%,45%) rotateY(0deg) rotateX(0deg);'
-        document.querySelector('.paintedsite').style= 'transform: translate(-190%,45%) rotateY(0deg) rotateX(0deg);'
-        document.querySelector('.intsite').style= 'transform:translate(-190%,-150%) rotateY(0deg) rotateX(0deg);'
-        document.querySelector('.reactsite').style= 'transform:translate(90%,-150%) rotateY(0deg) rotateX(0deg);'
-        document.querySelector('.gameprev').style= 'transform:translate(-50%,-150%) rotateY(0deg) rotateX(0deg);'
+        document.querySelector('.varepprev').style= 'transform: translate(-20%,-50%) rotateY(0deg) rotateX(0deg);'
+        document.querySelector('.paintedsite').style= 'transform: translate(20%,-50%) rotateY(0deg) rotateX(0deg);'
+        document.querySelector('.intsite').style= 'transform:translate(20%,50%) rotateY(0deg) rotateX(0deg);'
+        document.querySelector('.reactsite').style= 'transform:translate(-20%,50%) rotateY(0deg) rotateX(0deg);'
+        document.querySelector('.gameprev').style= 'transform:translate(-50%,50%) rotateY(0deg) rotateX(0deg);'
     })
 })
